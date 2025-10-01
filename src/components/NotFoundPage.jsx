6f8244dd43c91cbc69af77798fc9e9dbd00e0d56
Christian Reed <reed@Christians-MacBook-Pro.local>
2025-10-01 09:33:49 +0200

    added react-router-dom to the project

---
+++ src/components/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+export default function NotFoundPage() {
+	return (
+		<div>
+			<h1>Not Found Page ⊖</h1>
+			<Link to={'/'}>
+				<button>Go Back Home</button>
+			</Link>
+		</div>
+	);
+}
